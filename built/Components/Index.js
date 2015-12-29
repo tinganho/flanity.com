b5fb4a2711a06ebee91d2ce1dbb86c6145b2538a
Tingan Ho <tingan87@gmail.com>
2015-12-29 11:42:17 +0800

    Removed faulty folders

--- built/Components/Index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./ImageCrop'));
-__export(require('./SubmitButton'));
-__export(require('./FormMessage'));
-__export(require('./Link'));
-//# sourceMappingURL=Index.js.map
--- built/Start.js
+++
@@ -1,2 +0,0 @@
-require('./Server').start();
-//# sourceMappingURL=Start.js.map
